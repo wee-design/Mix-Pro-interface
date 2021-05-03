fd7ab93571441cf451e48f9eed3244e2e18712e9
Wibus <1596355173@qq.com>
2021-05-03 23:14:52 +0800

    add: github api & typecho page

--- github.php
+++ github.php
@@ -1,34 +1,28 @@
-<?$githubUser = 'wibus-wee';?>
-<script>
-  let httpRequest = new XMLHttpRequest();
-  httpRequest.open('GET',"https://api.github.com/users/<?php echo $githubUser; ?>/repos", true);
-  httpRequest.send();
-  let json = JSON.parse(httpRequest.responseText);
-</script>
 <?php
-                // $githubUser = $this->fields->github;
-                // if ($githubUser == "" || $githubUser == null){
-                    $githubUser = 'wibus-wee';
-                // }
-// $api = 'https://api.github.com/users/'.$githubUser; 
-// $file = file_get_contents($api);
-$json = json_decode($file);
-$login = $json->login;
-$avatar = $json->avatar;
-$name = $json->name;
-if ($name = 'null') {
-  $name = $githubUser;
+// $githubUser = $this->fields->github;
+if ($githubUser == "" || $githubUser == null){
+  $githubUser = 'wibus-wee';
 }
-$bio = $json->bio;
+  $opts = ['http' => ['method' => 'GET','header' => ['User-Agent: PHP']]];
+  $context = stream_context_create($opts);
+  $file = file_get_contents("https://api.github.com/users/$githubUser", false, $context);
+  // echo $file;
+  $json = json_decode($file);
+  $login = $json->login;
+  $avatar = $json->avatar;
+  $name = $json->name;
+  if ($name = 'null') {
+    $name = $githubUser;
+  }
+  $bio = $json->bio;
 
-// $api = 'https://api.github.com/users/'.$githubUser.'repos'; 
-// $file = file_get_contents($api);
-$file = 
-echo $file;
-$json = json_decode($file);
-foreach ($json as $item) {
-  $url = $item->url;
-  $language = $item->language;
-  echo $url."\n";
-  echo $language."\n";
-}
+  $file = file_get_contents("https://api.github.com/users/$githubUser/repos", false, $context);
+  $json = json_decode($file);
+  foreach ($json as $item) {
+      $url = $item->url;
+      $language = $item->language;
+      // echo $url."\n";
+      // echo $language."\n";
+  }
+  // echo $bio;
+  ?>
---
+++ github_typecho/index.php
@@ -0,0 +1,62 @@
+<?php
+// $githubUser = $this->fields->github;
+if ($githubUser == "" || $githubUser == null){
+  $githubUser = 'wibus-wee';
+}
+  $opts = ['http' => ['method' => 'GET','header' => ['User-Agent: PHP']]];
+  $context = stream_context_create($opts);
+  $file = file_get_contents("https://api.github.com/users/$githubUser", false, $context);
+  // echo $file;
+  $json = json_decode($file);
+  $login = $json->login;
+  $avatar = $json->avatar;
+  $name = $json->name;
+  if ($name == 'null') {
+    $name = $githubUser;
+  }
+  $bio = $json->bio;
+  $avatar_url = $json->avatar_url;
+  $file = file_get_contents("https://api.github.com/users/$githubUser/repos", false, $context);
+  $json = json_decode($file);
+  ?>
+
+
+<!DOCTYPE html>
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./index.css">
+    <script src="./index.js"></script>
+  </head>
+  <body>
+    <div class="page">
+      <div class="wrapper-inner">
+        <div class="primary">
+        <div class="view">
+    <img class="item" src="<?echo $avatar_url;?>">
+    </div>
+          <span class="name"><?echo $name;?></span><span class="login"><?echo $login;?></span><span class="bio-api"><?echo $bio;?></span>
+        </div>
+        <div class="sub-i0">
+<?
+
+  foreach ($json as $item) {
+    $body = '<div class="view-i0" style="background-color: rgb('.mt_rand(50, 255).', '.mt_rand(50, 255).', '.mt_rand(50, 255).')" >';
+    $url = $item->html_url;
+    $name = $item->name;
+    $description = $item->description;
+    // $language = $item->language;
+    $body .= '<span class="repo-name-2">'.$name.'</span><span class="description">'.$description.'</span><div class="mask"></div><a target="_blank" class="tag" href="'.$url.'">访问</a>';
+    // echo $url."\n";
+    // echo $language."\n";
+    $body .= '</div>';
+    echo $body;
+}
+
+?>
+      </div>
+    </div>
+
+
+</body></html>
